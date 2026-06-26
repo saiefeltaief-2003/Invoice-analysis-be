62e7426c714a1ab8ce2b3da38a272d355f61a613
saiefeltaief-2003 <saief.eltaief@gmail.com>
2026-06-26 11:30:53 +0100

    enabled cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173'], // Allowed frontends
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Required if sending cookies/auth headers
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
